f5be12e7100179073bc85d36e0cb20366c407057
Radiator Syrup <radiatorsyrup@gmail.com>
2021-08-23 13:06:03 +0100

    Initial

---
+++ __init__.py
@@ -0,0 +1,265 @@
+import bpy
+import subprocess
+import sys
+from collections import namedtuple
+from bpy.props import (
+    PointerProperty,
+    StringProperty,
+    BoolProperty,
+    IntProperty
+)
+
+from . splt_panel import SPLT_PT_Panel, SPLT_PT_warning_panel
+from . splt_ops import *
+
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+bl_info = {
+    "name": "SplatoonImageGenerator",
+    "author": "Radiator Syrup",
+    "description": "An addon to automate the rendering of Splatoon weapon models into 2d images",
+    "blender": (2, 80, 0),
+    "version": (0, 0, 1),
+    "location": "",
+    "warning": "",
+    "category": "Generic"
+}
+
+Dependency = namedtuple("Dependency", ["module", "package", "name"])
+
+# Declare all modules that this add-on depends on. The package and (global) name can be set to None,
+# if they are equal to the module name. See import_module and ensure_and_import_module for the
+# explanation of the arguments.
+dependencies = (Dependency(module="PIL", package="Pillow", name=None),
+                Dependency(module="numpy", package=None, name=None))
+
+
+def install_pip():
+    """
+    Installs pip if not already present. Please note that ensurepip.bootstrap() also calls pip, which adds the
+    environment variable PIP_REQ_TRACKER. After ensurepip.bootstrap() finishes execution, the directory doesn't exist
+    anymore. However, when subprocess is used to call pip, in order to install a package, the environment variables
+    still contain PIP_REQ_TRACKER with the now nonexistent path. This is a problem since pip checks if PIP_REQ_TRACKER
+    is set and if it is, attempts to use it as temp directory. This would result in an error because the
+    directory can't be found. Therefore, PIP_REQ_TRACKER needs to be removed from environment variables.
+    :return:
+    """
+
+    try:
+        # Check if pip is already installed
+        subprocess.run([sys.executable, "-m",
+                       "pip", "--version"], check=True)
+    except subprocess.CalledProcessError:
+        import os
+        import ensurepip
+
+        ensurepip.bootstrap()
+        os.environ.pop("PIP_REQ_TRACKER", None)
+
+
+def import_module(module_name, global_name=None):
+    """
+    Import a module.
+    :param module_name: Module to import.
+    :param global_name: (Optional) Name under which the module is imported. If None the module_name will be used.
+       This allows to import under a different name with the same effect as e.g. "import numpy as np" where "np" is
+       the global_name under which the module can be accessed.
+    :raises: ImportError and ModuleNotFoundError
+    """
+    import importlib
+
+    if global_name is None:
+        global_name = module_name
+
+    # Attempt to import the module and assign it to globals dictionary. This allow to access the module under
+    # the given name, just like the regular import would.
+    globals()[global_name] = importlib.import_module(module_name)
+
+
+def install_and_import_module(module_name, package_name=None, global_name=None):
+    """
+    Installs the package through pip and attempts to import the installed module.
+    :param module_name: Module to import.
+    :param package_name: (Optional) Name of the package that needs to be installed. If None it is assumed to be equal
+       to the module_name.
+    :param global_name: (Optional) Name under which the module is imported. If None the module_name will be used.
+       This allows to import under a different name with the same effect as e.g. "import numpy as np" where "np" is
+       the global_name under which the module can be accessed.
+    :raises: subprocess.CalledProcessError and ImportError
+    """
+    import importlib
+
+    if package_name is None:
+        package_name = module_name
+
+    if global_name is None:
+        global_name = module_name
+
+    # Try to install the package. This may fail with subprocess.CalledProcessError
+    subprocess.run([sys.executable, "-m", "pip",
+                   "install", package_name], check=True)
+
+    # The installation succeeded, attempt to import the module again
+    import_module(module_name, global_name)
+
+
+class SPLT_OT_install_dependencies(bpy.types.Operator):
+    bl_idname = "splatoon.install_dependencies"
+    bl_label = "Install dependencies"
+    bl_description = ("Downloads and installs the required python packages for this add-on. "
+                      "Internet connection is required. Blender may have to be started with "
+                      "elevated permissions in order to install the package")
+    bl_options = {"REGISTER", "INTERNAL"}
+
+    @classmethod
+    def poll(self, context):
+        # Deactivate when dependencies have been installed
+        return not context.window_manager.dependencies_installed
+
+    def execute(self, context):
+        print(sys.executable)
+        try:
+            install_pip()
+            for dependency in dependencies:
+                install_and_import_module(module_name=dependency.module,
+                                          package_name=dependency.package,
+                                          global_name=dependency.name)
+        except (subprocess.CalledProcessError, ImportError) as err:
+            self.report({"ERROR"}, str(err))
+            return {"CANCELLED"}
+
+        context.window_manager.dependencies_installed = True
+
+        # Register the panels, operators, etc. since dependencies are installed
+        for cls in classes:
+            bpy.utils.register_class(cls)
+
+        return {"FINISHED"}
+
+
+class SPLT_preferences(bpy.types.AddonPreferences):
+    bl_idname = __name__
+
+    def draw(self, context):
+        layout = self.layout
+        layout.operator(
+            SPLT_OT_install_dependencies.bl_idname, icon="CONSOLE")
+
+
+preference_classes = (SPLT_PT_warning_panel,
+                      SPLT_OT_install_dependencies,
+                      SPLT_preferences)
+classes = (RotateAndScale, PositionCamera,
+           PositionModel, FixMaterial, AddHRDI, RenderWiki, SPLT_PT_Panel)
+
+dependencies_installed = False
+
+
+def set_x_resolution(self, context):
+    context.scene.render.resolution_x = self.x_resolution
+
+
+def set_y_resolution(self, context):
+    context.scene.render.resolution_y = self.y_resolution
+
+
+def register():
+    global dependencies_installed
+    dependencies_installed = False
+
+    for cls in preference_classes:
+        bpy.utils.register_class(cls)
+
+    try:
+        for dependency in dependencies:
+            import_module(module_name=dependency.module,
+                          global_name=dependency.name)
+        dependencies_installed = True
+    except ModuleNotFoundError as e:
+        print(e)
+        # Don't register other panels, operators etc.
+        pass
+
+    if(dependencies_installed):
+        for cls in classes:
+            bpy.utils.register_class(cls)
+
+    # bpy.utils.register_class(SPLT_PT_Panel)
+
+    mode_options = [
+        ("JPEG", "JPEG", '', 'JPEG', 0),
+        ("PNG", "PNG", '', 'PNG', 1),
+
+    ]
+
+    bpy.types.WindowManager.output_format = bpy.props.EnumProperty(
+        items=mode_options,
+        description="File Format",
+        default=1,
+        name="Output Format"
+    )
+
+    bpy.types.WindowManager.objectselection_props = PointerProperty(
+        name="Base armature",
+        type=bpy.types.Object
+    )
+    bpy.types.WindowManager.x_rotations = IntProperty(
+        name="X Rotations",
+        default=36
+    )
+    bpy.types.WindowManager.y_rotations = IntProperty(
+        name="Y Rotations",
+        default=1
+    )
+    bpy.types.WindowManager.x_resolution = IntProperty(
+        name="X Resolution",
+        default=296,
+        update=set_x_resolution
+    )
+    bpy.types.WindowManager.y_resolution = IntProperty(
+        name="Y Resolution",
+        default=228,
+        update=set_y_resolution
+    )
+    bpy.types.WindowManager.output_folder = StringProperty(
+        name="Output Folder",
+        description="Path to Directory",
+        default="",
+        maxlen=1024,
+        subtype='DIR_PATH'
+    )
+
+    bpy.types.WindowManager.dependencies_installed = BoolProperty(
+        name="Dependencies installed",
+        default=dependencies_installed
+
+    )
+
+
+def unregister():
+    for cls in preference_classes:
+        bpy.utils.unregister_class(cls)
+
+    if dependencies_installed:
+        for cls in classes:
+            bpy.utils.unregister_class(cls)
+
+    del bpy.types.WindowManager.objectselection_props
+    del bpy.types.WindowManager.x_rotations
+    del bpy.types.WindowManager.y_rotations
+    del bpy.types.WindowManager.output_folder
+    del bpy.types.WindowManager.x_resolution
+    del bpy.types.WindowManager.y_resolution
+    del bpy.types.WindowManager.dependencies_installed
---
+++ splt_panel.py
@@ -0,0 +1,78 @@
+import bpy
+
+
+class SPLT_PT_Panel(bpy.types.Panel):
+    # bl_idname = "splatoonpanel"
+    bl_label = "Splatoon Tools"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "Splatoon Tools"
+
+    def draw(self, context):
+        layout = self.layout
+
+        box = layout.box()
+        box.label(text="Basic Settings")
+        box.prop(context.window_manager, "objectselection_props")
+
+        box.operator("object.rotate_and_scale")
+        box.operator("object.position_model")
+        box.operator("object.position_camera")
+        box.operator("object.fix_material")
+        box.operator("object.addhdri")
+
+        layout.separator()
+
+        box = layout.box()
+        box.label(text="Advanced Settings")
+
+        box.prop(context.window_manager, "x_rotations")
+
+        row = box.row()
+        row.prop(context.window_manager, "x_resolution")
+        row.prop(context.window_manager, "y_resolution")
+        box.prop(context.window_manager, "output_format")
+
+        layout.separator()
+        box = layout.box()
+        box.prop(context.window_manager, "output_folder")
+        box.operator("object.render_wiki")
+        layout.separator()
+
+        box = layout.box()
+
+        box.operator("wm.url_open", text="Manual",
+                     icon='HELP', emboss=True).url = "https://splatoonwiki.org/wiki/Inkipedia:3D_Models"
+
+
+class SPLT_PT_warning_panel(bpy.types.Panel):
+    bl_label = "Install Dependencies"
+    bl_category = "Splatoon Tools"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+
+    @classmethod
+    def poll(self, context):
+        return not context.window_manager.dependencies_installed
+
+    def draw(self, context):
+
+        layout = self.layout
+
+        lines = [f"Please install the missing dependencies for the PIL and numpy add-ons.",
+                 f"1. Open the preferences (Edit > Preferences > Add-ons).",
+                 f"2. Search for the Splatoon add-on.",
+                 f"3. Open the details section of the add-on.",
+                 f"4. Click on the Install button.",
+                 f"   This will download and install the missing Python packages, if Blender has the required",
+                 f"   permissions.",
+                 f"",
+                 f"If you're attempting to run the add-on from the text editor, you won't see the options described",
+                 f"above. Please install the add-on properly through the preferences.",
+                 f"1. Open the add-on preferences (Edit > Preferences > Add-ons).",
+                 f"2. Press the \"Install\" button.",
+                 f"3. Search for the add-on file.",
+                 f"4. Confirm the selection by pressing the \"Install Add-on\" button in the file browser."]
+
+        for line in lines:
+            layout.label(text=line)
